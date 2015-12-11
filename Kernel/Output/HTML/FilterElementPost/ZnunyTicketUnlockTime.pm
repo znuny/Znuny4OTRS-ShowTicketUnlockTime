80d01f51a7f4edb518e25bcd85848434c7b0ec53
Denny Bresch <db@znuny.com>
2015-12-11 10:48:41 +0100

    Refactoring OTRS 5 file structure.

--- Kernel/Output/HTML/FilterElementPost/ZnunyTicketUnlockTime.pm
+++ Kernel/Output/HTML/FilterElementPost/ZnunyTicketUnlockTime.pm
@@ -6,7 +6,7 @@
 # did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
 # --
 
-package Kernel::Output::HTML::OutputFilterPostZnunyTicketUnlockTime;
+package Kernel::Output::HTML::FilterElementPost::ZnunyTicketUnlockTime;
 
 use strict;
 use warnings;
